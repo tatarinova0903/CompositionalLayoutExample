90e5586c1f38d91b05627405c4d877bd0b9a0944
tatarinova999 <tatarinova999@yandex-team.ru>
2026-04-07 09:07:11 +0300

    empty project

---
+++ CompositionalLayoutExample/CompositionalLayoutExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CompositionalLayoutExample
+//
+//  Created by Daria Tatarinova on 07.04.2026.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
+
