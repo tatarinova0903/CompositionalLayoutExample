344dd7b681f21654c22905ac6fde5d3630fb26bb
tatarinova999 <tatarinova0903@yandex.ru>
2026-04-09 21:08:30 +0300

    real example template

---
+++ RealCompositionalLayout/RealCompositionalLayout/ViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class GridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+}
+
